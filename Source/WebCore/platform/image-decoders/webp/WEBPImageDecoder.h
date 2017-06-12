3155b137e2c5655661953fed470e38a7c21b7527
aansari <AzamAyub.Ansari@lnttechservices.com>
2017-06-12 16:39:50 -0400

    comcast webp animation support

--- Source/WebCore/platform/image-decoders/webp/WEBPImageDecoder.h
+++ Source/WebCore/platform/image-decoders/webp/WEBPImageDecoder.h
@@ -33,6 +33,7 @@
 #if USE(WEBP)
 
 #include "webp/decode.h"
+#include "webp/demux.h"
 
 namespace WebCore {
 
@@ -43,15 +44,28 @@ public:
 
     String filenameExtension() const override { return "webp"; }
     bool isSizeAvailable() override;
+    size_t frameCount() const override;
+    void setData(SharedBuffer& data, bool allDataReceived) override;
+    int repetitionCount() const override;
     ImageFrame* frameBufferAtIndex(size_t index) override;
 
 private:
-    bool decode(bool onlySize);
+    bool decode(size_t frameIndex, const WebPIterator* webpFrame, bool onlySize);
 
     WebPIDecoder* m_decoder;
-    bool m_hasAlpha;
+    WebPDecBuffer m_decoderBuffer;
+    int m_formatFlags;
+    bool m_frameBackgroundHasAlpha;
 
-    void applyColorProfile(const uint8_t*, size_t, ImageFrame&) { };
+    WebPDemuxer* m_demux;
+    WebPDemuxState m_demuxState;
+    bool m_haveAlreadyParsedThisData;
+    int m_repetitionCount;
+    int m_decodedHeight;
+
+    bool updateDemuxer();
+    bool initFrameBuffer(size_t frameIndex, const WebPIterator* webpFrame);
+    void applyPostProcessing(size_t frameIndex);
     void clear();
 };
 
