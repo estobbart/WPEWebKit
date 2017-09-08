a3ac7580205a71b0457b27c07f5e382d6582b10f
Eric Stobbart <Eric_Stobbart@comcast.com>
2017-09-08 14:53:57 -0700

    Viper WIP

--- Source/WebCore/platform/graphics/gstreamer/mse/MediaPlayerPrivateGStreamerMSE.h
+++ Source/WebCore/platform/graphics/gstreamer/mse/MediaPlayerPrivateGStreamerMSE.h
@@ -24,7 +24,7 @@
 
 #pragma once
 
-#if ENABLE(VIDEO) && USE(GSTREAMER) && ENABLE(MEDIA_SOURCE) 
+#if ENABLE(VIDEO) && USE(GSTREAMER) && ENABLE(MEDIA_SOURCE)
 
 #include "GRefPtrGStreamer.h"
 #include "MediaPlayerPrivateGStreamer.h"
@@ -133,7 +133,7 @@ private:
     RefPtr<MediaSourceClientGStreamerMSE> m_mediaSourceClient;
     MediaTime m_mediaTimeDuration;
     bool m_mseSeekCompleted = true;
-    RefPtr<PlaybackPipeline> m_playbackPipeline;
+    RefPtr<PlaybackPipeline> m_playbackPipeline; // Ref -> RefPtr?
 };
 
 } // namespace WebCore
